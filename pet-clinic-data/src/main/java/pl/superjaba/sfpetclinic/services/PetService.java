8665d5514fd87179dc49906d93cba22381680bc1
Karambix\Karambii <pichow@wp.pl>
2019-05-27 23:29:53 +0200

    refactored to common crud interface. closes #11

--- pet-clinic-data/src/main/java/pl/superjaba/sfpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/pl/superjaba/sfpetclinic/services/PetService.java
@@ -2,13 +2,6 @@ package pl.superjaba.sfpetclinic.services;
 
 import pl.superjaba.sfpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
